8ef2263033d330688d990199ff26c68b6ebf9c98
Joshua F. Robinson <joshuarrr@protonmail.com>
2022-04-07 13:19:17 +0200

    Caching to disk did not improve performance so disabled.

--- cache.py
+++ cache.py
@@ -3,15 +3,15 @@
 # Use @lru_cache decorator for caching directly in memory without disk.
 from functools import lru_cache
 
-# Create directory in the temp folder for caching to the disk.
-from pathlib import Path
-import tempfile
-source_path = Path(__file__).resolve()
-source_dir = source_path.parent
-source_project = source_dir.parts[-1]
-cachedir = Path('%s/%s' % (tempfile.gettempdir(), source_project))
+# # Create directory in the temp folder for caching to the disk.
+# from pathlib import Path
+# import tempfile
+# source_path = Path(__file__).resolve()
+# source_dir = source_path.parent
+# source_project = source_dir.parts[-1]
+# cachedir = Path('%s/%s' % (tempfile.gettempdir(), source_project))
 
-# Use @disk.memory decorator for caching to disk.
-from joblib import Memory
-#disk = Memory(cachedir, verbose=0)
+# # Use @disk.memory decorator for caching to disk.
+# from joblib import Memory
+# disk = Memory(cachedir, verbose=0)
 disk = None
