4e18376b47ddcb19b621b83bb057b67911feb34b
Joshua F. Robinson <joshuarrr@protonmail.com>
2024-07-03 10:58:24 +0100

    Revert to larger CUDA tile.

--- src/parameters.h
+++ src/parameters.h
@@ -32,8 +32,8 @@ namespace kernel
     // a CUDA thread for each tile sharing this memory. Varying the tile size
     // will potentially improve performance on different hardware - I found
     // 16x16 was close to optimum on my machine for simulations on a 1024x1024 grid.
-    static constexpr int tile_rows = 2;
-    static constexpr int tile_cols = 2;
+    static constexpr int tile_rows = 16;
+    static constexpr int tile_cols = 16;
     // We need ghost points for each tile so we can evaluate derivatives at tile borders.
     static constexpr int num_ghost = 1 + order / 2; // <- minimum for fourth derivatives
 }
